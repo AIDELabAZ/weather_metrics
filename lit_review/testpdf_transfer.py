10f9eb17b79bbbaaac5e972f613a92087170e5cc
KieranCDouglas <kieran@arizona.edu>
2025-06-23 08:55:29 -0700

    words mod
    - wording changes

--- lit_review/testpdf_transfer.py
+++ lit_review/testpdf_transfer.py
@@ -1,6 +1,6 @@
 ###
 ## This script matches filenames from the 15% test CSV to those of the PDFs in the full PDF folder
-## Matched PDF files are transfered to a new folder callede pdf_test_15
+## Matched PDF files are copied to a new folder callede pdf_test_15
 ###
 
 import os
